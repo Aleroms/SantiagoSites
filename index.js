bed5cfd5f19baf4dc1d3549efe9af64d4ab9e60c
Aleroms <santiagomorales333@yahoo.com>
2022-09-11 23:11:22 -0700

    added projects section

--- index.js
+++ index.js
@@ -2,7 +2,16 @@ $(document).ready(function(){
     $('#slider').hide();
     $('.project-card-overlay').hide();
 
+    //toggles the buger menu for mobile view
     $('#burger').on('click', function(){
         $('#slider').slideToggle(420);
     });
+
+    //toggles overlay for projects
+    $('.projects-card').hover(function(){
+        let overlay = $(this).find('.project-card-overlay');
+        $(overlay).toggle(420);
+
+    });
+
 });
